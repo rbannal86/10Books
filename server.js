885bc93d0b016493c3ca39aa37c04d75269d22d9
Connor Stec <stec.connor@gmail.com>
2020-02-26 16:19:05 -0500

    populate server boilerplate, modify package.json, add gitignore

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const app     = express();
+const PORT    = 8000;
+
+// implement your gbooks functionality and route(s) here!
+
+app.listen(PORT);
+
+console.log("Listening on port:", PORT);
