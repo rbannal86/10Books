bedd75c7e09cc2feeec14a91fdde9b0880c44c99
Connor Stec <stec.connor@gmail.com>
2020-02-26 16:38:27 -0500

    Add pg dep

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app     = express();
 const PORT    = 8000;
+const { Client, Pool } = require('pg');
 
 // implement your gbooks functionality and route(s) here!
 
